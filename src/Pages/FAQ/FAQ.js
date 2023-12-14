0f82f6f5ed7e05c2c4d4698e52301d18734df59d
qwertyNippon <orlandohenry100@gmail.com>
2023-12-13 23:22:01 -0500

    update styles, set up react

---
+++ src/Pages/FAQ/FAQ.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { styled } from "styled-components";
+
+function Button() {
+    
+    return (
+        <ButtonStyled >
+
+        </ButtonStyled>
+    )
+}
+
+const ButtonStyled = styled.button`
+    display: flex;
+    align-items: center;
+`;
+
+export default Button
