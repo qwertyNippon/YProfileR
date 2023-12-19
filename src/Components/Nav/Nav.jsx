80a373f77da3ec996230a70568091de371a2df05
qwertyNippon <orlandohenry100@gmail.com>
2023-12-18 22:48:14 -0500

    testing format

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -5,7 +5,25 @@ function Button() {
     
     return (
         <ButtonStyled >
-
+            <header>
+                <div class="container">
+                    <nav>
+                        <div class="logo">
+                            <a href="#"><span>Yuri</span> Japan</a>
+                        </div>
+                        <ul>
+                            <li><a href="https://yport.onrender.com/">Home</a></li>
+                            <li><a href="#">Profile</a></li>
+                            <li><a href="#">Courses</a></li>
+                            <li><a href="#">Contact</a></li>
+                            <li><a href="#">FAQ</a></li>
+                        </ul>
+                        <button class="hamburger">
+                            <div class="bar"></div>
+                        </button>
+                    </nav>
+                </div>
+            </header>
         </ButtonStyled>
     )
 }
