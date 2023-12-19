3f1f4e5d479a0452a7e45641513c33c14206d9fb
qwertyNippon <orlandohenry100@gmail.com>
2023-12-18 23:01:14 -0500

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-
+import Nav from './Components/Nav/Nav'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Nav />
     </>
   )
 }
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,36 +1,141 @@
 import React from "react";
 import { styled } from "styled-components";
 
-function Button() {
+function Nav() {
     
     return (
-        <ButtonStyled >
+        <NavStyled >
             <header>
-                <div class="container">
+                <div className="container">
                     <nav>
-                        <div class="logo">
+                        <div className="logo">
                             <a href="#"><span>Yuri</span> Japan</a>
                         </div>
                         <ul>
-                            <li><a href="https://yport.onrender.com/">Home</a></li>
-                            <li><a href="#">Profile</a></li>
-                            <li><a href="#">Courses</a></li>
-                            <li><a href="#">Contact</a></li>
-                            <li><a href="#">FAQ</a></li>
+                            <Link><a href="https://yport.onrender.com/">Home</a></Link>
+                            <Link><a href="#">Profile</a></Link>
+                            <Link><a href="#">Courses</a></Link>
+                            <Link><a href="#">Contact</a></Link>
+                            <Link><a href="#">FAQ</a></Link>
                         </ul>
-                        <button class="hamburger">
-                            <div class="bar"></div>
-                        </button>
+                        <Nav className="hamburger">
+                            <div className="bar"></div>
+                        </Nav>
                     </nav>
                 </div>
             </header>
-        </ButtonStyled>
+        </NavStyled>
     )
 }
 
-const ButtonStyled = styled.button`
+const NavStyled = styled.Nav`
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Rubik', sans-serif;
+}
+
+:root {
+    --text-color: #00fbff;
+    /* --text-color: pink; */
+    --bg-color: #323234;
+    /* --bg-color: #b4b4d4; */
+    --white: #fff;
+    --black: #000;
+    --mob-color: #444444;
+}
+
+.container {
+    max-width: 1250px;
+    width: 100%;
+    margin: 0 auto;
+}
+
+header {
+    background: var(--bg-color);
+    height: 90px;
     display: flex;
     align-items: center;
+    justify-content: center;
+}
+
+nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+nav .logo {
+    z-index: 1;
+}
+
+nav .logo a {
+    text-decoration: none;
+    font-size: 32px;
+    font-weight: 600;
+    color: var(--white);
+}
+
+nav .logo a span {
+    color: var(--text-color);
+}
+
+nav ul {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 35px;
+    font-size: 18px;
+    list-style: none;
+    z-index: 1;
+}
+
+nav ul li a {
+    text-decoration: none;
+    color: var(--white);
+    transition: 0.4s ease;
+}
+
+nav ul li a:hover {
+    color: var(--text-color);
+}
+
+nav .hamburger {
+    position: relative;
+    width: 30px;
+    cursor: pointer;
+    appearance: none;
+    background: none;
+    outline: none;
+    border: none;
+    display: none;
+    scale: 0.8;
+}
+
+nav .hamburger .bar,
+nav .hamburger::after,
+nav .hamburger::before {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 3px;
+    background: var(--white);
+    margin: 6px 0;
+    transition: 0.4s;
+}
+
+nav .hamburger.active::before {
+    transform: rotate(-45deg) translate(-8px, 5px);
+}
+
+nav .hamburger.active::after {
+    transform: rotate(45deg) translate(-8px, -5px);
+}
+
+nav .hamburger.active .bar {
+    opacity: 0;
+}
 `;
 
-export default Button
+export default Nav
