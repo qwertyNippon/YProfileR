22d6764b99bea0add004160d17781f3401423926
qwertyNippon <orlandohenry100@gmail.com>
2024-01-02 21:32:22 -0500

    update

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -6,7 +6,33 @@ import axios from "axios";
 
 
 function SignUp() {
-    
+    const [email, setEmail] = useState()
+    const [userName, setUserName] = useState()
+    const [password, setPassword] = useState()
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const form = e.target;
+        console.log(form);
+        let vals = {};
+        vals['email']= e.target[0].value;
+        vals['username']= e.target[1].value;
+        vals['pass'] = e.target[2].value;
+        console.log(vals);
+        axios.post('http://127.0.0.1:5000/signup', JSON.stringify(vals), {
+            // when sight goes live the above address needs to change
+            headers: { "Content-Type": "application/json" }
+        }
+        )
+        .then(function (response) {
+            // console.log(response);
+            navi('/login')
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
     return (
         <SignUpStyled >
 
