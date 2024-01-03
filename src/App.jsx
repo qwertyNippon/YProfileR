ce2b414c4009952cfc66ea69b8c0afc1c0061a0b
qwertyNippon <orlandohenry100@gmail.com>
2024-01-02 21:41:28 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { GlobalStyle } from './Styles/GlobalStyle'
 import Nav from './Components/Nav/Nav'
 import Footer from './Components/Footer/Footer'
-import { Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +10,10 @@ function App() {
   return (
     <>
       <Nav />
-      <Route />
+      <Routes>
+        <Route children path='/login' element={<Login  />} />
+        <Route children path='/signup' element={<Signup  />} />
+      </Routes>
       <Footer />
     </>
   )
