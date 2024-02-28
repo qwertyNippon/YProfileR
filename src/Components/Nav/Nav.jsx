80198c13a00ef27c93385519bc3ed62cc550af79
qwertyNippon <orlandohenry100@gmail.com>
2024-02-27 20:30:19 -0500

    working on one form of the nav

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -22,44 +22,48 @@ function Nav() {
       }
     
     return (
-            <header>
-                <div className="container">
-                    <nav>
-                        <div className="logo">
-                            <a href="#"><span>Logo</span></a>
-                        </div>
-                        <ul>
-                            <Link><a href="{{ url_for('home') }}">Dashboard</a></Link>
-                            <Link><a href="#">Find a Teacher</a></Link>
-                        </ul>
-                        <hr />
-                        <ul>
-                            <Link><a href="#">Profile</a></Link>
-                            <Link><a href="#">Courses</a></Link>
-                            <Link><a href="#">Contact</a></Link>
-                            <Link><a href="#">FAQ</a></Link>
-                             {user ?  
-                            <>
-                            <Link><a href="https://yport.onrender.com/">YuriPro</a></Link>
-                            
-                            </>
-                             : null} 
-                            
-                            {!user ? null : 
-
+        <>
+            {/* {user ? */}
+                <header>
+                    <div className="container">
+                        <nav>
+                            <div className="logo">
+                                <a href="#"><span>Logo</span></a>
+                            </div>
+                            <ul>
+                                <Link><a href="{{ url_for('home') }}">Dashboard</a></Link>
+                                <Link><a href="#">Find a Teacher</a></Link>
+                            </ul>
+                            <hr />
+                            <ul>
+                                <Link><a href="#">Profile</a></Link>
+                                <Link><a href="#">Courses</a></Link>
+                                <Link><a href="#">Contact</a></Link>
+                                <Link><a href="#">FAQ</a></Link>
                                 <>
-                                <li>
-                                    <div className="logsign">
-                                    <Link className="textD" to="/" onClick={()=>logout()}>LOGOUT</Link>
-                                    </div>
-                                </li>
+                                    <Link><a href="https://yport.onrender.com/">YuriPro</a></Link>
+
                                 </>
-                            }
-                        </ul>
-                    </nav>
-                </div>
-            </header>
-        
+
+                            </ul>
+                        </nav>
+                    </div>
+                </header>
+                {/* : null}  */}
+
+                
+                    {/* {!user ? null : 
+                        
+                        <>
+                        <li>
+                        <div className="logsign">
+                        <Link className="textD" to="/" onClick={()=>logout()}>LOGOUT</Link>
+                        </div>
+                        </li>
+                        </>
+                    } */}
+                    
+        </>
     );
 }
 
