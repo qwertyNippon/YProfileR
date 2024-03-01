3b3be5ee63091e15ef5ce5103b35c35587a0cc4f
qwertyNippon <orlandohenry100@gmail.com>
2024-02-29 22:09:46 -0500

    updates

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -15,7 +15,7 @@ function Footer() {
                             <p className="makeCenter">
                                 Copyright &copy; 2024 All Rights Reserved by
                                 <div className="column space">
-                                    <a href="#">YURI JAPAN</a>
+                                    <a href="https://yurikaclasses.onrender.com/" target="_blank">YURI JAPAN</a>
                                     &nbsp;&nbsp;&nbsp;
                                     <a href="https://www.instagram.com/yuri_japan_japanese_education_?igsh=anRsM3YweG43cjNx&utm_source=qr&fbclid=IwAR0KfRjanxLNF1PD7HV5bnSmivmAm_1IQTiuo4qXe3cZfcIVxZZPGVf3_AY" target="_blank" class="fa-brands fa-instagram"></a>
                                     &nbsp;&nbsp;&nbsp;
