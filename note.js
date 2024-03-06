7a8f4fa2eafb37a0ed917d78d6fd89525c8c6ef1
qwertyNippon <orlandohenry100@gmail.com>
2024-03-06 16:38:28 -0500

    updates

--- note.js
+++ note.js
@@ -1,7 +1,4 @@
 // work on ProviderLayer.jsx and DataProvider.jsx
 
 // start to build the front end signin etc. pages
-// Fixed the issue of react not showing up.
-// Next start building pages
-// Work on login and sign up
 // work various navbars for diferent pages
