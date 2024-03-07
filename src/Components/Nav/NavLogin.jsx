77899ee818841ff9f7b25e4a8cbddc3c2a40d21f
qwertyNippon <orlandohenry100@gmail.com>
2024-03-06 23:03:45 -0500

    updates

--- src/Components/Nav/NavLogin.jsx
+++ src/Components/Nav/NavLogin.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import './NavLogin.css'
+import Logo from '../../assets/Logo.png'
 
 function NavLogin() {
     
     return (
         < >
-
+            <div className="container">
+                <div className="logo">
+                    <a href="#"><img className="LogoPic" src={Logo} alt="Logo" /></a>
+                </div>
+            </div>
         </>
     )
 }
