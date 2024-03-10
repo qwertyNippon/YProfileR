a26413b77e7dc72fc8d9c73e4fa47069b72659e2
qwertyNippon <orlandohenry100@gmail.com>
2024-03-10 00:00:38 -0500

    updates

--- src/Components/Nav/NavLogin.jsx
+++ src/Components/Nav/NavLogin.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './NavLogin.css'
 import Logo from '../../assets/Logo.png'
 
@@ -6,11 +7,19 @@ function NavLogin() {
     
     return (
         < >
-            <div className="container">
+            <div className="containerNavLogin">
                 <div className="logo">
                     <a href="#"><img className="LogoPic" src={Logo} alt="Logo" /></a>
                 </div>
+                <ul className="menu">
+                    <li><Link to='#'>Find tutors</Link></li>
+                    <li><Link to='#'>Become a tutor</Link></li>
+                </ul>
+                <div className="log_butt_right_container">
+                    <button className="friendButt log_butt_right"><Link to="/login"><i className="fa-solid fa-arrow-right-to-bracket"></i> Log in</Link></button>
+                </div>
             </div>
+            <hr />
         </>
     )
 }
