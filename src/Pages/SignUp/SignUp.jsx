615e95627aa8dd3374cad46ab82c569af80c48a2
qwertyNippon <orlandohenry100@gmail.com>
2024-03-10 00:35:35 -0500

    updates

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -58,7 +58,7 @@ function SignUp() {
                         <input type="text" placeholder="Username" className="input-field" />
                         <input type="password" placeholder="Password" className="input-field" />
                         <input type="password" placeholder="Confirm Password" className="input-field" />
-                        <button type="submit" className="submit-button">Login</button>
+                        <button type="submit" className="submit-button">Sign up</button>
                     </div>
 
                     <div className="bottomText space">By clicking Log in or Continue with, you agree to Preply <Link><a href="#" className="TOS">Terms of Use</a></Link> and <Link><a href="#" className="TOS">Privacy Policy</a></Link></div>
