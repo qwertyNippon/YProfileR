689cd923a366af702ccbe7008ea41839375408ca
qwertyNippon <orlandohenry100@gmail.com>
2024-03-10 00:18:22 -0500

    updates

--- note.js
+++ note.js
@@ -1,6 +1,3 @@
 // work on ProviderLayer.jsx and DataProvider.jsx
 
-// start to build the front end signin etc. pages
-// work various navbars for diferent pages
-
-
+// working on calendar
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -35,12 +35,36 @@ function SignUp() {
 
     return (
         < >
-            Signup
-            <div className="login-form">
-                <input type="email" placeholder="Email" className="input-field" />
-                <input type="text" placeholder="Username" className="input-field" />
-                <input type="password" placeholder="Password" className="input-field" />
-                <button type="submit" className="submit-button">Login</button>
+            <div className="makeCenter top">
+                <div className="container_login makeCenter">
+                    <div className="Login makeCenter space">Sign up</div>
+
+                    <div className="choose ">
+                        <Link className=""><a href="#">Sign up as tutor</a></Link>
+                        <div className="">or</div>
+                        <Link className=""><a href="#">Sign up as a student</a></Link>
+                    </div>
+
+                    <div>INSERT GOOGLE AND OTHER LOGINS</div>
+
+                    <div class="parent ">
+                        <hr className="grid-hr" />
+                        <div className="grid-item">or</div>
+                        <hr className="grid-hr" />
+                    </div>
+                    
+                    <div className="login-form">
+                        <input type="email" placeholder="Email" className="input-field" />
+                        <input type="text" placeholder="Username" className="input-field" />
+                        <input type="password" placeholder="Password" className="input-field" />
+                        <input type="password" placeholder="Confirm Password" className="input-field" />
+                        <button type="submit" className="submit-button">Login</button>
+                    </div>
+
+                    <div className="bottomText space">By clicking Log in or Continue with, you agree to Preply <Link><a href="#" className="TOS">Terms of Use</a></Link> and <Link><a href="#" className="TOS">Privacy Policy</a></Link></div>
+
+
+                </div>
             </div>
         </>
     )
