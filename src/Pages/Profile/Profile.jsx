a7be3565c4a8013c1bc03a6c3c082e53c49253bf
qwertyNippon <orlandohenry100@gmail.com>
2024-04-09 21:37:54 -0400

    updates

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,44 +1,34 @@
 import React from "react";
-// import './Profile.css'
+import './Profile.css'
 
 function Profile() {
     
     return (
         <>
-            <header>
-                <div class="container">
-                    <nav>
-                        <div class="logo">
-                            <a href="#"><span>UserName</span></a>
-                        </div>
-                    </nav>
-                </div>
-            </header>
-
-            <section class="home">
-                <div class="container">
-                    <div class="home-info">
-                        <div class="left">
-                            <h3>Hello, I'm</h3>
-                            <h1>Yurika Henry</h1>
-                            <h4>
-                                I am a  <span class="multiple"></span>
-                            </h4>
-                            <p>
-                                insert details Lorem,  ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-                            </p>
-                            <div class="btn">
-                                <button>Download...</button>
+            <div className='makeCenter'>
+                <div className="headerProfile">
+                    <div className="container">
+                        <nav>
+                            <div className="logo">
+                                <a href="#"><span>UserName</span></a>
                             </div>
-                        </div>
-                        <div class="right">
-                            <div class="profile">
-                                <img src="images/profile.jpg" alt=""/>
+                        </nav>
+                    </div>
+                </div>
+
+                <section className="home">
+                    <div className="container">
+                        <div className="home-info">
+                            <div className="makeCenter">
+                                <h3>Price</h3>
+                                <h3>Introduce yourself</h3>
+                                <h3>Language experience</h3>
+                                <h1>Certificates/Qualifications</h1>
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            </div>
         </>
     )
 }
