7ce0ef5a9e2728a29c4da51cb3cf4a8b2f6d9396
qwertyNippon <orlandohenry100@gmail.com>
2024-04-22 22:49:50 -0400

    updates

--- src/Components/Language/LanguageSelection.jsx
+++ src/Components/Language/LanguageSelection.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useTranslation } from "react-i18next";
 import i18next from 'i18next';
-
+import NavPass from '../Nav/NavPass';
 import './Language.css';
 
 
 const LanguageSelection = () => {
     // Default language and level based on browser language
-    const fallbackLanguage = i18next.options.fallbackLng[0]; // Extract language code
+    var fallbackLanguage = i18next.options.fallbackLng[0]; // Extract language code
     const [languages, setLanguages] = useState([{ language: fallbackLanguage, level: 'Native' }]);
     const [newLanguage, setNewLanguage] = useState('');
     const [newLevel, setNewLevel] = useState('');
@@ -41,8 +41,8 @@ const LanguageSelection = () => {
 
     return (
         <div>
-            <h2>Language Selection</h2>
-            <ul>
+            <h2 className='makeCenter'>Language Selection</h2>
+            <ul className='listLan makeCenter'>
                 {languages.map((lang, index) => (
                     <li key={index}>
                         {lang.language} - {lang.level}
@@ -51,23 +51,24 @@ const LanguageSelection = () => {
                     </li>
                 ))}
             </ul>
-            <div>
-                <input
+            <div className=''>
+                <input className='text-inputLan'
                     type="text"
                     placeholder="Language"
                     value={newLanguage}
                     onChange={(e) => setNewLanguage(e.target.value)}
                 />
-                <input
+                <input className='text-inputLan'
                     type="text"
                     placeholder="Level"
                     value={newLevel}
                     onChange={(e) => setNewLevel(e.target.value)}
                 />
-                <button onClick={addLanguage}>Add Language</button>
+                <button onClick={addLanguage}>{t('addAnLan')}</button>
             </div>
         </div>
     );
 };
 
 export default LanguageSelection;
+
