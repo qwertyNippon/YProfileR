0125de29dbb54d034c9b969703d876522e68850f
qwertyNippon <orlandohenry100@gmail.com>
2024-05-16 22:20:35 -0400

    update

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -77,6 +77,16 @@ function Profile() {
       };
 
     //   js for pictures upload video 10mins in
+    const dropArea = document.getElementById('drop-area');
+    const inputFile = document.getElementById('input-file');
+    const imageView = document.getElementById('img-view');
+
+    inputFile.addEventListener('change', uploadImage);
+
+    function uploadImage(){
+        let imgLink = URL.createObjectURL(inputFile.files[0]);
+        imageView.style.backgroundImage = `url(${imgLink})`;
+    }   
 
     return (
         <>
